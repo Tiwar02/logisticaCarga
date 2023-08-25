c6f080fcdf0698642e28f49051022f5d96bed117
Tiwar02 <salazarstiwar@gmail.com>
2023-08-24 21:52:45 -0500

    Correcion Status Code

--- tests/trailers.spec.js
+++ tests/trailers.spec.js
@@ -7,7 +7,7 @@ describe('GET /trailers', () => {
     test('should respond with a 200 status code', async () => {
 
         const response = await request(app).get('/trailers').send()
-        expect(response.statusCode).toBe(201);
+        expect(response.statusCode).toBe(200);
     })
 
     test('should respond with an array', async () => {
